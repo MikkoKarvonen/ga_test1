32c773a9a28b34b51089922e9c7c7be6fdfceee5
MikkoKarvonen <mkarvonen34@gmail.com>
2024-09-16 14:30:08 +0300

    Add Netlify link

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <a href="#" target="_blank" rel="noreferrer">
-        Change url
+      <a href="https://ga-test2.netlify.app" target="_blank" rel="noreferrer">
+        Netlify
       </a>
     </div>
   );
