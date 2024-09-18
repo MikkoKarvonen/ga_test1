ac0813f154ab8b592265f4ff959f1e924e273a7f
MikkoKarvonen <mkarvonen34@gmail.com>
2024-09-18 08:32:53 +0300

    Add space to link

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,15 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Link href="https://ga-test2.netlify.app" rel="noreferrer">
-        Netlify (Link)
-      </Link>
-      <a href="https://ga-test2.netlify.app" rel="noreferrer">
-        Netlify (a)
-      </a>
+      <div>
+        <Link href="https://ga-test2.netlify.app" rel="noreferrer">
+          Netlify (Link)
+        </Link>
+        <span className="p-10" />
+        <a href="https://ga-test2.netlify.app" rel="noreferrer">
+          Netlify (a)
+        </a>
+      </div>
     </div>
   );
 }
